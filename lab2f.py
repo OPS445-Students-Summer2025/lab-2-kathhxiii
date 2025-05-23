a505b8dbe7fd792734ce81cddb61a4ec057066bf
Kathleen Pacanut <kpacanut@myseneca.ca>
2025-05-23 16:07:12 -0400

    Add lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Kathleen Pacanut
+# Author ID: kpacanut
+# Date Created: 2025/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
